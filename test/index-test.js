3e9a4f7612d6f2406a5e2832a3c75c3e889277b8
Crisson Jno-Charles <crissonjc@gmail.com>
2016-05-18 18:56:35 -0400

    support for mocha tests

---
+++ test/index-test.js
@@ -0,0 +1,8 @@
+describe('quickrest', function () {
+  it('is a function')
+
+  it('returns an object')
+  it('throws an error if `endpoints` is an empty array')
+
+  it('throws an error if `root` is undefined')
+})
